080d65dcd5b7e4cdba80d8fe0c13ecdd2b82267f
Hieu Nguyen <nthieu2501@gmail.com>
2022-05-07 19:46:13 +0700

    test

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 mkdir tokenization_repair
-cd tokenization_repair
+mkdir tokenization_repair/unilm
+mkdir tokenization_repair/blm
+
+mkdir model
+
+cd tokenization_repair/unilm
 gdown https://drive.google.com/drive/folders/1zhtQmPTah7qneEHPHuFHuf3qd5R047-W -O / --folder
+cd ../blm
 gdown https://drive.google.com/drive/folders/1lG3swcUyUPYOf4ziJGfPQboaYtD5SgTM -O / --folder
+
+cd ../../model
+gdown https://drive.google.com/uc?id=1PqGIjiQCp5xNINsAcBzOracLw0t6CuVA
+gdown https://drive.google.com/uc?id=145geEupadzGwxZaueZE-kr4fHYmO-REC
