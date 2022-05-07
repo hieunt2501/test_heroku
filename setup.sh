f998d7325f3a33ff06830e5e7423c849ce802c36
Hieu Nguyen <nthieu2501@gmail.com>
2022-05-07 18:57:11 +0700

    change setting

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 mkdir tokenization_repair
 cd tokenization_repair
-mkdir data/estimators/
-mkdir data/estimators/lm/
-cd data/estimators
 gdown https://drive.google.com/drive/folders/1zhtQmPTah7qneEHPHuFHuf3qd5R047-W -O / --folder
-cd ../lm
 gdown https://drive.google.com/drive/folders/1lG3swcUyUPYOf4ziJGfPQboaYtD5SgTM -O / --folder
